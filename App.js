f41f97cbb23625ccb7321ca1bef96d9f23d42862
spriyanka2919 <66846210+spriyanka2919@users.noreply.github.com>
2020-11-27 17:42:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CartItem from './CartItem';
+import Cart from './Cart';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Cart/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ CartItem.js
@@ -0,0 +1,104 @@
+import React from 'react';
+class CartItem extends React.Component{
+  
+     
+    //testing(){
+    //    const promise = new Promise ((resolve, reject) => {
+      //      setTimeout(() => {
+        //        resolve('done');
+          //  }, 5000);
+       // })
+
+     // promise.then(() => {
+          //setState acts like a synchronus call
+       //   this.setState({ qty: this.state.qty +10 });
+
+         // console.log('state',this.state);
+     // });
+   // }
+
+    increaseQuantity = () => {
+      //  this.state.qty +=1;
+       // console.log('this',this.state);
+             //setStaate form 1
+           //  this.setState({
+             //    qty: this.state.qty + 1
+            // } ,() => {});
+
+            //setState form 2 -if prevState required use this
+            this.setState((prevState) => {
+               return{
+                   qty:prevState.qty +1
+               }
+            } 
+             );
+        }
+
+    decreaseQuantity = () => {
+
+
+        const { qty }= this.state;
+        if(qty===0){
+            return;
+        }
+        //setState form 2 -if prevState required use this
+        this.setState((prevState) => {
+            return{
+                qty:prevState.qty - 1
+            }
+         });
+
+ }
+
+
+
+
+    render(){
+        console.log('this.props',this.props);
+        const{ price,title,qty}=this.props.product ;
+        return(
+          <div className="cart-item" >
+              <div className="left-block">
+                     <img style={styles.image}/>
+               </div>
+                   <div className="right-block">
+                   <div style={{fontSize:25}}>{title}</div>
+                  <div style={{color:'#777'}}>Rs {price}</div>
+                   <div style={{color:'#777'}}>Qty: {qty}</div>
+                   <div className="cart-item-actions">
+                       {/*Buttons*/}
+                       
+                       <img alt="increase" 
+                       className="action-icons" 
+                       src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"
+                       onClick={this.increaseQuantity}
+                       />
+                       
+                       <img alt="decrease"
+                        className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"
+                        onClick={this.decreaseQuantity}
+                        />
+                       
+                       <img alt="delete" 
+                       className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg"
+                       />
+
+                   </div>
+
+               </div>
+
+          </div>
+        );
+    }
+}
+
+const styles={
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem;
